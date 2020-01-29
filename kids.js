022f520840d2867964500b58eea28bba41037d4c
Hagar Ahmed <44428106+HagarAhmed97@users.noreply.github.com>
2020-01-29 05:29:30 -0800

    Kids Town Templete

---
+++ kids.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#flipw").click(function(){
+    $("#panelw").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#flipd").click(function(){
+    $("#paneld").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#fliph").click(function(){
+    $("#panelh").slideToggle("slow");
+  });
+});
